202ac0e1f9303794bacdada6d5ca9c46404d41e9
TsuruPong <50530188+TsuruPong@users.noreply.github.com>
2024-08-22 00:17:26 +0900

    add: gql client settings

---
+++ src/libs/apollo/client.tsx
@@ -0,0 +1,14 @@
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+
+export const apolloClient = new ApolloClient({
+  uri: "/api/graphql",
+  cache: new InMemoryCache(),
+});
+
+export const ApolloClientProvider = ({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) => {
+  return <ApolloProvider client={apolloClient}>{children}</ApolloProvider>;
+};
