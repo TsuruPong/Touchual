36bed289b3d932ff4625107e9541e893b1489ceb
TsuruPong <50530188+TsuruPong@users.noreply.github.com>
2024-10-09 23:07:57 +0900

    Update resolver.ts

--- src/libs/graphql/resolver.ts
+++ src/libs/graphql/resolver.ts
@@ -1,4 +1,3 @@
-import { Prisma } from "@prisma/client"
 import prisma from "../prisma/client"
 
 interface result {
@@ -11,17 +10,19 @@ export const resolvers = {
     Query: {
         getApproxSentence: async(_: any, args: { level: number, difficulty: number }) => {
             const result = await prisma.$queryRaw<result[]>`
-                    SELECT
-                    text,
-                    ruby
-                    FROM
+                select
+                *
+                from
+                "SentenceIndicators"
+                where
+                abs(difficulty - ${args.difficulty}) = (
+                    select
+                    min(abs(difficulty - ${args.difficulty})) as d
+                    from
                     "SentenceIndicators"
-                    WHERE
-                    level = ${args.level}
-                    AND
-                    difficulty = ${args.difficulty}
-                    LIMIT 1
-                `;
+                )
+                and level = ${args.level}
+            `;
             const { text, ruby, level, difficulty } = result[0];
             return {text, ruby, level, difficulty };
        }
