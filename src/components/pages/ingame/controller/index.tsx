504d63fd3c357f2878aff01a9c101f72ca1fd8fb
TsuruPong <50530188+TsuruPong@users.noreply.github.com>
2024-12-16 23:46:41 +0900

    Update index.tsx

--- src/components/pages/ingame/controller/index.tsx
+++ src/components/pages/ingame/controller/index.tsx
@@ -12,7 +12,6 @@ import { useTokenizer } from "./hook/useTokenizer";
 import { MoraWithStatus } from "./type/extends/mora";
 import { useMoraTypeConverter } from "./hook/useMoraTypeConverter";
 import { useUpdateMora } from "./hook/useUpdateMora";
-import { AutoCompleate } from "../presentation/autocompleate/type";
 
 export const InGameContainer: React.FC = () => {
     const [sentence, setSentence] = React.useState<{
@@ -23,12 +22,9 @@ export const InGameContainer: React.FC = () => {
     const { inputs, clear } = useKeyboardInput();
     const { convertJaToRomajiTokens } = useTokenizer();
     const { isTypingCorrect } = useTypingValidator();
-    const [moras, setMoras] = React.useState<MoraWithStatus[]>();
+    const [moras, setMoras] = React.useState<MoraWithStatus[]>([]);
     const { toMoraWithStatus, toAutoCompleate } = useMoraTypeConverter();
     const { updateCorrect, updateIncorrect } = useUpdateMora();
-    const [autoCompleate, setAutoCompleate] = React.useState<AutoCompleate[]>(
-        []
-    );
     //const [level, setLevel] = React.useState<number>(1);
     //const [difficulty, setDifficulty] = React.useState<number>(1.0);
     const { totalCounter, correctCounter, incorrectCounter } =
@@ -48,12 +44,18 @@ export const InGameContainer: React.FC = () => {
     }, []);
 
     React.useEffect(() => {
+        let ignore = false;
         const sentence = data?.getApproxSentence || undefined;
         if (!sentence?.text && !sentence?.ruby) return;
-        setSentence(() => ({
-            text: sentence.text,
-            ruby: sentence.ruby,
-        }));
+        if (!ignore) {
+            setSentence(() => ({
+                text: sentence.text,
+                ruby: sentence.ruby,
+            }));
+        }
+        return () => {
+            ignore = true;
+        };
     }, [loading, data]);
 
     const convertTokens = React.useCallback(() => {
@@ -67,46 +69,53 @@ export const InGameContainer: React.FC = () => {
         convertTokens();
     }, [convertTokens]);
 
-    React.useEffect(() => {
-        if (!moras || moras.length == 0) return;
-        setAutoCompleate(() => toAutoCompleate(moras));
-        if (moras.every((m) => m.status == "correct")) {
-            refetch({
-                level: 2,
-                difficulty: 2.2,
-            });
-            return;
-        }
-    }, [moras]);
+    if (moras.length != 0 && moras.every((m) => m.status == "correct")) {
+        refetch({
+            level: 2,
+            difficulty: 2.2,
+        });
+    }
+
+    if (inputs.some((key) => key.code == "Escape")) {
+        backward();
+        return;
+    }
 
     React.useEffect(() => {
         if (inputs.length == 0) return;
-        if (inputs.some((key) => key.code == "Escape")) {
-            backward();
+        if (
+            !moras ||
+            moras.length == 0 ||
+            moras.every((m) => m.node.length == 0) ||
+            moras.every((m) => m.status == "correct")
+        ) {
             return;
         }
-        if (!moras || moras.length == 0) return;
+        let change = false;
         let m: MoraWithStatus[] = [...moras];
         for (const input of inputs) {
             totalCounter.increment();
             if (isTypingCorrect(moras, input.key)) {
                 correctCounter.increment();
                 m = updateCorrect(m, input.key);
+                change = true;
             } else {
                 incorrectCounter.increment();
                 m = updateIncorrect(m);
+                change = true;
                 break;
             }
         }
-        console.log(m);
-        setMoras(() => m);
+        if (change) {
+            setMoras(() => m);
+        }
         clear();
     }, [inputs]);
 
     return (
         <InGamePresentation
             sentence={sentence}
-            autocompleates={autoCompleate}
+            autocompleates={toAutoCompleate(moras)}
             handleTimeup={forward}
             handleRefetch={(level: number, difficulty: number) =>
                 refetch({
