eed5b5d9d2d26e5bdfb24efd73ee875d501518b4
TsuruPong <50530188+TsuruPong@users.noreply.github.com>
2025-01-08 22:21:00 +0900

    Update useTypingHandler.ts

--- src/components/pages/ingame/controller/hook/useTypingHandler.ts
+++ src/components/pages/ingame/controller/hook/useTypingHandler.ts
@@ -54,6 +54,11 @@ const validator = () => {
         const targetMora = moras.find(mora => mora.status != "correct");
         if (!targetMora?.node) throw new Error("illegal parameter");
         const targetMoraNodes = findTargetMoraNodeRecursively(targetMora.node);
+        const prevMora = moras.find(mora => mora.pos = targetMora.pos - 1);
+        if (prevMora?.from == "っ" && isShortConsonant(prevMora.node)) {
+            const symbom = getShortConsonantSymbol(prevMora.node);
+            return input == symbom.val == targetMoraNodes.some(moranode => moranode.val == input);
+        }
         return targetMoraNodes.some(moranode => moranode.val == input);
     }
     const findTargetMoraNodeRecursively = (nodes: MoraNodeWithStatus[]): MoraNodeWithStatus[] => {
@@ -63,6 +68,14 @@ const validator = () => {
         } else {
             return nodes;
         }
+    }
+    const isShortConsonant = (nodes: MoraNodeWithStatus[]): boolean => {
+        return nodes.find(node => node.status == "correct")?.children.length == 0;
+    }
+    const getShortConsonantSymbol = (nodes: MoraNodeWithStatus[]): MoraNodeWithStatus => {
+        const symbol = nodes.find(node => node.status == "correct");
+        if (!symbol) throw new Error(`illegal parameter`);
+        return symbol;
     }
     return { isTypingCorrect }
 }
