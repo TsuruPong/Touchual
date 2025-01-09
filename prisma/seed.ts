2d63c8332ef85502bf20232b7d123cb38ae70fab
TsuruPong <50530188+TsuruPong@users.noreply.github.com>
2025-01-09 23:16:37 +0900

    Update seed.ts

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -15,49 +15,49 @@ async function main() {
             text: "赤パジャマ青パジャマ気パジャマ",
             ruby: "あかぱじゃまあおぱじゃまきぱじゃま",
             level: 1,
-            difficulty: 1.0
+            difficulty: 0
         },
         {
             text: "生麦生米生卵",
             ruby: "なまむぎなまごめなまたまご",
             level: 1,
-            difficulty: 1.1
+            difficulty: 0.1
         },
         {
             text: "隣の客はよく柿食う客だ",
             ruby: "となりのきゃくはよくかきくうきゃくだ",
             level: 1,
-            difficulty: 1.2
+            difficulty: 0.2
         },
         {
             text: "すももも桃ももものうち桃もすももも桃のうち",
             ruby: "すもももももももものうちもももすももももものうち",
             level: 2,
-            difficulty: 2.0
+            difficulty: 0
         },
         {
             text: "新人歌手新春シャンソンショー",
             ruby: "しんじんかしゅしんしゅんしゃんそんしょー",
             level: 2,
-            difficulty: 2.1
+            difficulty: 0.1
         },
         {
             text: "坊主が屏風に上手に坊主の絵を描いた",
             ruby: "ぼうずがびょうぶにじょうずにぼうずのえをかいた",
             level: 2,
-            difficulty: 2.2
+            difficulty: 0.2
         },
         {
             text: "竹藪に竹立てかけた",
             ruby: "たけやぶにたけたてかけた",
             level: 3,
-            difficulty: 3.0
+            difficulty: 0
         },
         {
             text: "抜きにくい釘引き抜きにくい釘釘抜きで抜く釘",
             ruby: "ぬきにくいくぎひきぬきにくいくぎくぎぬきでぬくくぎ",
             level: 3,
-            difficulty: 3.1
+            difficulty: 0.1
         },
     ];
     await prisma.sentenceIndicators.deleteMany({});
